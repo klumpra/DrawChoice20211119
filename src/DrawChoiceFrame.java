ab1711a48ec3452208e4e0889a294c85d22b8c07
klumpra <klumpra@lewisu.edu>
2021-11-19 17:16:46 -0600

    Added event handlers

--- src/DrawChoiceFrame.java
+++ src/DrawChoiceFrame.java
@@ -1,12 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 public class DrawChoiceFrame extends JFrame {
+	private DrawChoicePanel dcp;
 	public void setupGUI() {
 		setTitle("Draw Choice Frame");
 		setBounds(100,100,300,300);
@@ -16,11 +18,27 @@ public class DrawChoiceFrame extends JFrame {
 		JPanel panSouth = new JPanel();
 		panSouth.setLayout(new FlowLayout());
 		JButton btnCircle = new JButton("Circle");
+		btnCircle.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dcp.setShapeType(0);
+						repaint();
+					}
+				}
+		);
 		panSouth.add(btnCircle);
 		JButton btnSquare = new JButton("Square");
+		btnSquare.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dcp.setShapeType(1);
+						repaint();
+					}
+				}
+		);
 		panSouth.add(btnSquare);
 		c.add(panSouth,BorderLayout.SOUTH);
-		DrawChoicePanel dcp = new DrawChoicePanel();
+		dcp = new DrawChoicePanel();
 		c.add(dcp,BorderLayout.CENTER);
 	}
 	public DrawChoiceFrame() {
